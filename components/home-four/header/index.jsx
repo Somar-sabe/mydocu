74ba766664babe6d9568099163b44f8f2ea8738e
tech-aatienda <techteam@aatienda.com>
2024-12-23 12:42:34 +0400

    esddsaadssdsahgfaa

--- components/home-four/header/index.jsx
+++ components/home-four/header/index.jsx
@@ -22,17 +22,7 @@ function Header() {
 					<HeaderLogo />
 					<div className="menu-block-wrapper">
 						<DesktopNav>
-							<NavItem dropdown title="Demo">
-								<Dropdown>
-									<DropdownItem url="/">Design Agency</DropdownItem>
-									<DropdownItem url="home-two">Startup Agency</DropdownItem>
-									<DropdownItem url="home-three">SEO Agency</DropdownItem>
-									<DropdownItem url="home-four">Business Consultation</DropdownItem>
-									<DropdownItem url="home-five">Digital Marketing</DropdownItem>
-									<DropdownItem url="home-six">Interior Design Agency</DropdownItem>
-									<DropdownItem url="home-seven">Advertising agency</DropdownItem>
-								</Dropdown>
-							</NavItem>
+						<NavItem url="/">Home</NavItem>
 							<NavItem url="about-us">About Us</NavItem>
 							<NavItem dropdown title="Pages">
 								<Dropdown>
